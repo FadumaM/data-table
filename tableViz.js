2e093b1b9971ca9217f8ccdfb7f03dac80000b1f
FadumaM <faduma.mohammed@gmail.com>
2016-08-13 19:53:27 +0100

    created table and data had been added to it

--- tableViz.js
+++ tableViz.js
@@ -1,4 +1,35 @@
-tableViz = function (elm, data) {
-	console.log(elm);
-	console.log(data);
-}
+tableViz = function(elm, data) {
+  $(document).ready(function() {
+    var $header = $('.view_count_table_header');
+    bindEventListener($header);
+    addDataOverallTable();
+  });
+
+  function bindEventListener($header) {
+    var turn = 1;
+    $header.on('click', function() {
+      turn *= -1;
+      var length = $(this).prevAll().length;
+      sortTable(turn, length);
+    });
+  }
+
+
+
+  function addDataOverallTable() {
+    $.map(data, function(month, index) {
+      $.each(month, function(channel, figure) {
+        $('.view_count_table_body tr').each(function() {
+          var $channelHeading = $(this);
+          var $channelHeadingId = $(this).attr('id');
+          if ($channelHeadingId === channel) {
+            $channelHeading.append('<td>' + figure +
+              '</td>');
+          }
+        });
+      });
+    });
+
+  }
+
+};
